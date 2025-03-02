f6504eba0dd69c2b3a2941844fa17d788a965bb1
jfegasu <jfegasu@gmail.com>
2025-03-02 11:31:37 -0500

    .

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
 from flask import Flask , render_template
-=======
-from flask import Flask,render_template 
->>>>>>> Stashed changes
+
 from flask_cors import CORS
 from admin.routes import admin 
 from park.routes import parq
@@ -18,5 +15,11 @@ app.register_blueprint(pisc)
 @app.route("/")
 def raiz():
     return render_template("index.html")
+@app.route("/banner")
+def banner():
+    return render_template("banner.html")
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
 if __name__=='__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
