9f1dfd9326f234020aa1a40edbb76af8b4585224
jfegasu <jfegasu@gmail.com>
2025-03-08 20:31:21 -0500

    .

--- 4-DESARROLLO/index.py
+++ 4-DESARROLLO/index.py
@@ -2,12 +2,18 @@ from flask import Flask,Blueprint, render_template
 import pandas as pd
 from flask_cors import CORS
 from admin.routes import admin 
+from entrada.routes import entra 
+from park.routes import parq 
+from piscina.routes import pisc 
 # importar el Blueprint 
 # Crear la aplicación Flask 
 app = Flask(__name__) 
 
 # Registrar el Blueprint en la aplicación 
+app.register_blueprint(entra)
 app.register_blueprint(admin)
+app.register_blueprint(parq)
+app.register_blueprint(pisc)
 @app.route('/') 
 def raiz(): 
     return render_template('index.html')
@@ -31,5 +37,6 @@ def centro():
 @app.route('/footer') 
 def footer(): 
     return render_template('footer.html')
+
 if __name__=='__main__':
     app.run(debug=True,port=5000,host='0.0.0.0')
