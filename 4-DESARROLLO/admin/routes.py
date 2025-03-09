d92fcce54e45139e0d7d41db2823bea66366984a
jfegasu <jfegasu@gmail.com>
2025-03-08 21:03:39 -0500

    .

--- 4-DESARROLLO/admin/routes.py
+++ 4-DESARROLLO/admin/routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template 
+import pandas as pd
 # Crear un Blueprint 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
 # Definir rutas y vistas dentro del Blueprint 
 @admin.route('/') 
 def raiz(): 
-    return 'MODULO DE ADMINISTRACION'
+    menu = []
+    a = pd.read_csv('admin/menu.csv')
+
+    # Convert all rows to a list of lists (faster approach)
+    menu = a.values.tolist()
+
+    return render_template('menu.html',menus=menu)
